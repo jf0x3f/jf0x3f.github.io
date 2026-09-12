fc703ea85b584fe0d5ae855a605efc5d557728d0
jf0x3a <johnfiel@plxrmedia.com>
2026-09-12 22:15:07 +0800

    a

--- wp-content/themes/cybercentauri 2/footer.php
+++ wp-content/themes/cybercentauri 2/footer.php
@@ -1,7 +1,7 @@
 <footer class="cc-footer">
   <a class="cc-brand" href="#top"><span class="cc-mark">&gt;_</span><span>CYBERCENTAURI<br><em>~/SECURITY_RESEARCH</em></span></a>
   <p><span class="cc-footer-prompt">$</span> Independent research for responsible defenders.</p>
-  <nav><a href="#writeups">WRITEUP SECTORS</a><a href="#contact">EMAIL</a></nav>
+  <nav><a href="<?php echo esc_url(home_url('/#writeups')); ?>">WRITEUP SECTORS</a><a href="<?php echo esc_url(home_url('/#contact')); ?>">EMAIL</a></nav>
   <span>© <?php echo esc_html(date('Y')); ?> / EOF_</span>
 </footer>
 </div>
